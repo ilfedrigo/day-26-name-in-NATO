558b1252a62a464317fd16f07cecd84fe01a2fe1
Lucas Fedrigo <contato.fedrigo@gmail.com>
2024-02-01 20:09:33 -0300

    Implementing try and except when user enter only numbers

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pandas
 
 data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
-nato_dict = {row.letter:row.code for (index, row) in data_frame.iterrows()}
-word = input("Enter a word: ").upper()
 
-output_code = [nato_dict[letter] for letter in word]
-print(output_code)
+nato_dict = {row.letter:row.code for (index, row) in data_frame.iterrows()}
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_code = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("You can only use letters on the alphabet.")
+        generate_phonetic()
+    else:
+        print(output_code)
+
+generate_phonetic()
